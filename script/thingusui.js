4f34779b8423b824423dfe3400697fdb88e58982
sverweij <481@nepserver.fake>
2013-09-05 22:09:28 +0200

    add ga tags

--- script/thingusui.js
+++ script/thingusui.js
@@ -1,18 +1,40 @@
 define(["xaja", "jquery"],
     function(xaja, $) {
 
-var RUNNINGLOCAL = true;
+var RUNNINGLOCAL = false;
 var x;
 $(document).ready(function(){
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-43809800-1', 'sverweij.github.io');
+    ga('send', 'pageview');
+    
+    $("#submit").bind({
+        click : function(e) {
+                    ga('send', 'event', 'submit', $("#baseurl").val());    
+                }
+    });
+    $("a").bind({
+        click : function(e) {
+                    var x = e.currentTarget? (e.currentTarget.href? e.currentTarget.href: "unknown") : "unknown";
+                    console.debug(x);
+                    ga('send', 'event', 'linkclick', x);    
+        }
+    });
     $("#PNRList").bind("change", function () {PNRListOnChange();});
     $("#baseurl").bind("change", function () {baseurlOnChange();});
     try {
         xaja.makeJSONRequest("./pnrs.json", parsePNRs, RUNNINGLOCAL);
+        ga('send', 'event', 'pnrlist_shown', 'true');
     } catch (e) {
         $("#PNRList").hide();
+        ga('send', 'event', 'pnrlist_shown', 'false');
     }
 
-})
+});
 
 function baseurlOnChange(){
     $("#manual").attr("action", $("#baseurl").val());
@@ -79,4 +101,4 @@ function parsePNRs (pData) {
     }
 }
 
-})
+});
