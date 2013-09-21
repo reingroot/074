cc25ebd20aa76c7aeb96a232125aae2cd9fec03e
sverweij <481@nepserver.fake>
2013-09-21 12:23:30 +0200

    replace homebrewn ajax wrapper with jquery one

--- script/thingusui.js
+++ script/thingusui.js
@@ -1,107 +1,113 @@
-define(["xaja", "jquery"],
-    function(xaja, $) {
+define(["jquery"], function($) {
 
-var RUNNINGLOCAL = ("file:" === window.location.protocol);
-var x;
-$(document).ready(function(){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    var gPNRList;
+    $(document).ready(function() {
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] ||
+            function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', 'UA-43809800-1', 'sverweij.github.io');
-    ga('send', 'pageview');
-    
-    $("#submit").bind({
-        click : function(e) {
-                    ga('send', 'event', 'submit', $("#baseurl").val());    
-                }
+        ga('create', 'UA-43809800-1', 'sverweij.github.io');
+        ga('send', 'pageview');
+
+        $("#submit").bind({
+            click : function(e) {
+                ga('send', 'event', 'submit', $("#baseurl").val());
+            }
+        });
+        $("a").bind({
+            click : function(e) {
+                var x = e.currentTarget ? (e.currentTarget.href ? e.currentTarget.href : "unknown") : "unknown";
+                ga('send', 'event', 'linkclick', x);
+            }
+        });
+        $("#PNRList").bind("change", function() {
+            PNRListOnChange();
+        });
+        $("#baseurl").bind("change", function() {
+            baseurlOnChange();
+        });
+
+        $.getJSON('./pnrs.json', parsePNRs);
+
     });
-    $("a").bind({
-        click : function(e) {
-                    var x = e.currentTarget? (e.currentTarget.href? e.currentTarget.href: "unknown") : "unknown";
-                    ga('send', 'event', 'linkclick', x);    
+
+    function baseurlOnChange() {
+        $("#manual").attr("action", $("#baseurl").val());
+    }
+
+    function PNRListOnChange() {
+        var i = $("#PNRList").val();
+        /* init non-mandatory elements */
+        $("#description").text("");
+
+        $("#bookingcode").val(gPNRList.reservations[i].bookingcode);
+        $("#lastname").val(gPNRList.reservations[i].lastname);
+        if (gPNRList.reservations[i].description) {
+            $("#description").text(gPNRList.reservations[i].description);
         }
-    });
-    $("#PNRList").bind("change", function () {PNRListOnChange();});
-    $("#baseurl").bind("change", function () {baseurlOnChange();});
-    try {
-        xaja.makeJSONRequest("./pnrs.json", parsePNRs, RUNNINGLOCAL);
-        ga('send', 'event', 'pnrlist_shown', 'true');
-    } catch (e) {
-        $("#PNRList").hide();
-        ga('send', 'event', 'pnrlist_shown', 'false');
-    }
-
-});
-
-function baseurlOnChange(){
-    $("#manual").attr("action", $("#baseurl").val());
-}
-
-function PNRListOnChange () {
-    var i = $("#PNRList").val();
-    /* init non-mandatory elements */
-    $("#description").text("");
-
-    $("#bookingcode").val(x.reservations[i].bookingcode);
-    $("#lastname").val(x.reservations[i].lastname);
-    if (x.reservations[i].description) {
-        $("#description").text(x.reservations[i].description);
-    } 
-    if (x.reservations[i].country) {
-        $("#POS").val(x.reservations[i].country);
-        $("#COUNTRY").val(x.reservations[i].country);
-    } 
-    if (x.reservations[i].language) {
-        $("#LANG").val(x.reservations[i].language);
-    } 
-     if (x.reservations[i].entryreason||""===x.reservations[i].entryreason){
-        $("#entryreason").val(x.reservations[i].entryreason);
-    } 
-    if (x.reservations[i].landingpage||""===x.reservations[i].landingpage){
-        $("#landingpage").val(x.reservations[i].landingpage);
-    } 
-    if (x.reservations[i].viewproduct||""===x.reservations[i].viewproduct){
-        $("#viewproduct").val(x.reservations[i].viewproduct);
-    } 
-    if (x.reservations[i].skipheader||""===x.reservations[i].skipheader){
-        $("#skipHeader").val(x.reservations[i].skipheader);
-    } 
-
-}
-
-function compareReservation(pReservation1, pReservation2) {
-    if (pReservation1.lastname > pReservation2.lastname) {
-        return 1;
-    }
-    if (pReservation1.lastname < pReservation2.lastname) {
-        return -1;
-    }
-    return 0;
-}
-
-function parsePNRs (pData) {
-    try {
-        x = JSON.parse(pData);
-        var totalDuration = 0;
-
-        $("#PNRList").fadeIn("slow");
-        $("#PNRList").empty();
-        x.reservations.sort(compareReservation);
-        for (var i=0;i<x.reservations.length;i++) {
-            $("#PNRList").append("<option value=\"" + i +  "\">" +
-                    x.reservations[i].lastname + "/ " +
-                    x.reservations[i].bookingcode +
-                    /*
-                    "<i> (" + x.reservations[i].description + ") </i>" + 
-                    */
-                    "</option>");
+        if (gPNRList.reservations[i].country) {
+            $("#POS").val(gPNRList.reservations[i].country);
+            $("#COUNTRY").val(gPNRList.reservations[i].country);
         }
-        PNRListOnChange();
-    } catch (e) {
-        $("#PNRList").hide();
+        if (gPNRList.reservations[i].language) {
+            $("#LANG").val(gPNRList.reservations[i].language);
+        }
+        if (gPNRList.reservations[i].entryreason || "" === gPNRList.reservations[i].entryreason) {
+            $("#entryreason").val(gPNRList.reservations[i].entryreason);
+        }
+        if (gPNRList.reservations[i].landingpage || "" === gPNRList.reservations[i].landingpage) {
+            $("#landingpage").val(gPNRList.reservations[i].landingpage);
+        }
+        if (gPNRList.reservations[i].viewproduct || "" === gPNRList.reservations[i].viewproduct) {
+            $("#viewproduct").val(gPNRList.reservations[i].viewproduct);
+        }
+        if (gPNRList.reservations[i].skipheader || "" === gPNRList.reservations[i].skipheader) {
+            $("#skipHeader").val(gPNRList.reservations[i].skipheader);
+        }
+
     }
-}
+
+    function compareReservation(pReservation1, pReservation2) {
+        if (pReservation1.lastname > pReservation2.lastname) {
+            return 1;
+        }
+        if (pReservation1.lastname < pReservation2.lastname) {
+            return -1;
+        }
+        return 0;
+    }
+
+    function parsePNRs(pData) {
+
+        try {
+            gPNRList = pData;
+
+            $("#PNRList").fadeIn("slow");
+            $("#PNRList").empty();
+            pData.reservations.sort(compareReservation);
+            for (var i = 0; i < gPNRList.reservations.length; i++) {
+                $("#PNRList").append("<option value=\"" + i + "\">" + gPNRList.reservations[i].lastname + "/ " + gPNRList.reservations[i].bookingcode +
+                /*
+                 "<i> (" + x.reservations[i].description + ") </i>" +
+                 */
+                "</option>");
+            }
+            PNRListOnChange();
+            ga('send', 'event', 'pnrlist_shown', 'true');
+
+        } catch (e) {
+            $("#PNRList").hide();
+            ga('send', 'event', 'pnrlist_shown', 'false');
+
+        }
+    }
 
 });
